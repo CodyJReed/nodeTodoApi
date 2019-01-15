c51721fc2ebf419c8bd5ab238493698ace817373
Cody Reed <cody2j2reed@gmail.com>
2019-01-15 13:24:20 -0500

    Create mongodb-update.js

---
+++ playground/mongodb-update.js
@@ -0,0 +1,13 @@
+const { MongoClient, ObjectID } = require("mongodb");
+
+MongoClient.connect(
+  "mongodb://localhost:27017/TodoApp",
+  (err, db) => {
+    if (err) {
+      return console.log("Unable to connect MongoDB server");
+    }
+    console.log("Connected to MongoDB server");
+
+    // db.close();
+  }
+);
