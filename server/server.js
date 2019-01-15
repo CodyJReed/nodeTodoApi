db9a8892a45af45c466d03dc354c3bfa441934a7
Cody Reed <cody2j2reed@gmail.com>
2019-01-15 16:32:48 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
+// Local Imports
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Library Imports
+
+// Mongoose
 const { mongoose } = require("./db/mongoose");
 
 // Collection Model(s)/Schema(s)
 const { Todo } = require("./models/todo");
 const { User } = require("./models/user");
+
+const app = express();
+
+app.listen("3000", () => {
+  console.log("Started on port 3000");
+});
