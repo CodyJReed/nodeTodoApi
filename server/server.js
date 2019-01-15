1e341e73f21dea3ec820e408eea96366c90aa459
Cody Reed <cody2j2reed@gmail.com>
2019-01-15 14:01:40 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,3 +2,15 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/TodoApp");
+
+const Todo = mongoose.model("Todos", {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
