05aa490a5730720beeb61ab3ad3bff967b1e3762
Cody Reed <cody2j2reed@gmail.com>
2019-01-16 14:55:29 -0500

    Added tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -16,7 +16,7 @@ beforeEach(done => {
 });
 
 // Testing POST "/todos" route
-describe("POST/todos", () => {
+describe("POST /todos", () => {
   it("should create a new todo", done => {
     const text = "Test todo text";
 
@@ -64,4 +64,14 @@ describe("POST/todos", () => {
   });
 });
 
-describe("GET/todos", () => {});
+describe("GET /todos", () => {
+  it("should get all todos", done => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
+});
