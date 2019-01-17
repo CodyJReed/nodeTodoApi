56469b35f1045cf8af37d3f9428a2f8d64b36260
Cody Reed <cody2j2reed@gmail.com>
2019-01-17 14:07:40 -0500

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { ObjectID } = require("mongodb");
+const _ = require("lodash");
 
 // Library Imports
 
@@ -82,6 +83,34 @@ app.delete("/todos/:id", (req, res) => {
     })
     .catch(err => {
       return res.status(400).send();
+    });
+});
+
+// Update Todo(s)
+app.patch("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ["text", "completed"]);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  // Set completedAt
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(err => {
+      res.status(400).send();
     });
 });
 
