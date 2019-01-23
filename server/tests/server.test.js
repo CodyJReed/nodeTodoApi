7a1d5718a6b7e5d475ae4e431800d61c7b603b88
Cody Reed <cody2j2reed@gmail.com>
2019-01-23 14:02:43 -0500

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -254,7 +254,7 @@ describe("POST /users", () => {
   });
 });
 
-describe("POST /users/login", done => {
+describe("POST /users/login", () => {
   it("should login user and return auth token", done => {
     request(app)
       .post("/users/login")
@@ -301,5 +301,32 @@ describe("POST /users/login", done => {
           })
           .catch(e => done(e));
       });
+  });
+});
+
+describe("DELETE /users/me/token", () => {
+  it("should remove user token on logout", done => {
+    request(app)
+      .delete("/users/me/token")
+      .set("x-auth", users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[0]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+
+  it("should reject invalid logout", done => {
+    request(app)
+      .delete("/users/me/token")
+      .expect(401)
+      .end(done);
   });
 });
